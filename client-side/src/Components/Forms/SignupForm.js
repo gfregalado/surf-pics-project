8df32423c163a6e03c5f57d8b243ce0bd1ecc9f4
Gonçalo Regalado <gf.regalado@gmail.com>
2020-06-15 21:09:19 +0100

    Changes form structure to use Antd, still needs to update handle with radio buttons to change user type image

--- client-side/src/Components/Forms/SignupForm.js
+++ client-side/src/Components/Forms/SignupForm.js
@@ -1,91 +1,94 @@
 import React from 'react';
-import { Formik } from 'formik';
 
-import surfIcon from '../../Assets/Icons/surf-equipment.svg';
-import photoIcon from '../../Assets/Icons/photo-camera.svg';
-import CustomSwitch from '../../Components/UI/Switch/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Classes from './SignupForm.module.css';
+// import Classes from './SignupForm.module.css';
 
-const SignupForm = (props) => (
-  <div>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
+import { Form, Input, Radio } from 'antd';
+import Button from '../UI/Buttons/Button';
+
+const SignupForm = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <Form
+      name="signup_form"
+      className="login-form"
+      onFinish={onFinish}
+      style={{ marginLeft: '50px', marginRight: '50px', marginTop: '60px' }}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <label For="LastName">Email</label>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          ></input>
-          <div className={Classes.ErrorBox}>
-            {errors.email && touched.email && errors.email}
-          </div>
+      <Form.Item
+        name="firstName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your First name',
+          },
+        ]}
+      >
+        <Input type="firstName" placeholder="First Name" />
+      </Form.Item>
+      <Form.Item
+        name="lastName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Last name',
+          },
+        ]}
+      >
+        <Input type="LasttName" placeholder="Last Name" />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input placeholder="Email" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input type="password" placeholder="Password" />
+      </Form.Item>
 
-          <label For="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          ></input>
-          <p>{errors.password && touched.password && errors.password}</p>
+      <Form.Item
+        name="radio-group"
+        // label="Yo, I am a"
+        rules={[
+          {
+            required: true,
+            message: 'Please Choose which one are you!',
+          },
+        ]}
+      >
+        <Radio.Group>
+          <Radio value="a">Surfer</Radio>
+          <Radio value="b">Photographer</Radio>
+        </Radio.Group>
+      </Form.Item>
 
-          <div className={Classes.Switch}>
-            <img
-              src={photoIcon}
-              alt="surfIcon"
-              width="40px"
-              style={{ paddingRight: '27px' }}
-            />
-            <FormControlLabel
-              control={<CustomSwitch onChange={props.switch} name="userType" />}
-            />
-            <img src={surfIcon} alt="surfIcon" width="40px" />
-          </div>
-
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            style={{ marginRight: '30%', width: '50%' }}
-          >
-            Create Account
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+      <Form.Item>
+        <Button htmlType="submit" className="login-form-button">
+          Create Account
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default SignupForm;
