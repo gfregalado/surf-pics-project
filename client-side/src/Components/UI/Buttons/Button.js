554f6506cf869ae45c4bec810f512fd8e9433826
Gonçalo Regalado <gf.regalado@gmail.com>
2020-06-15 11:21:43 +0100

    refactors navbar to mobile first

--- client-side/src/Components/UI/Buttons/Button.js
+++ client-side/src/Components/UI/Buttons/Button.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Classes from './Button.module.css';
-import Button from '@material-ui/core/Button';
 
 const button = (props) => {
   let classname = props.buttonSecondary ? Classes.Secondary : Classes.Primary;
