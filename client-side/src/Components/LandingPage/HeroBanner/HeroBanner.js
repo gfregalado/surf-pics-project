c241a14704100dd0f26c60b559822dfa004b97fc
Gonçalo Regalado <gf.regalado@gmail.com>
2020-06-15 11:58:09 +0100

    refactor hero banner and buttons to mobile first

--- client-side/src/Components/LandingPage/HeroBanner/HeroBanner.js
+++ client-side/src/Components/LandingPage/HeroBanner/HeroBanner.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import classes from './HeroBanner.module.css';
+import Classes from './HeroBanner.module.css';
+import Button from '../../UI/Buttons/Button';
+import { Link } from 'react-router-dom';
 
 const navbar = () => {
   return (
-    <div className={classes.Container}>
-      <div className={classes.Content}>
-        <div className={classes.Header}>
+    <div className={Classes.Container}>
+      <div className={Classes.Content}>
+        <div className={Classes.Header}>
           <h1>SURF PICS PROJECT</h1>
           <h3>Your Water Sports Marketplace</h3>
         </div>
-        <div className={classes.Description}>
+        <div className={Classes.Description}>
           <h3>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </h3>
+          <div className={Classes.Buttons}>
+            <Link to="/signup">
+              <Button>Signup</Button>
+            </Link>
+            <Button buttonSecondary>Login</Button>
+          </div>
         </div>
       </div>
-      <div className={classes.Image}></div>
+      <div className={Classes.Image}></div>
     </div>
   );
 };
--- client-side/src/Components/UI/Buttons/Button.js
+++ client-side/src/Components/UI/Buttons/Button.js
@@ -3,11 +3,8 @@ import Classes from './Button.module.css';
 
 const button = (props) => {
   let classname = props.buttonSecondary ? Classes.Secondary : Classes.Primary;
-  return (
-    <button style={{ marginTop: '0', marginBottom: '0' }} className={classname}>
-      {props.children}
-    </button>
-  );
+  return <button className={classname}>{props.children}</button>;
 };
 
 export default button;
+// style={{ marginTop: '0', marginBottom: '0' }}
