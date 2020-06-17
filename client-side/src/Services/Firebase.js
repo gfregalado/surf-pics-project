0d00c377df22e7b2d694e9ce1024d204a8abef46
Gonçalo Regalado <gf.regalado@gmail.com>
2020-06-17 11:59:41 +0100

    fixes gitignore not working

--- client-side/src/Services/Firebase.js
+++ client-side/src/Services/Firebase.js
@@ -1,11 +1,11 @@
 let firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: 'surf-pic-project.firebaseapp.com',
-  databaseURL: 'https://surf-pic-project.firebaseio.com',
-  projectId: 'surf-pic-project',
-  storageBucket: 'surf-pic-project.appspot.com',
-  messagingSenderId: '235753154623',
-  appId: '1:235753154623:web:f35cd1e85447383513308a',
-  measurementId: 'G-30P1MW5KQT',
+  authDomain: 'surf-pics-marketplace.firebaseapp.com',
+  databaseURL: 'https://surf-pics-marketplace.firebaseio.com',
+  projectId: 'surf-pics-marketplace',
+  storageBucket: 'surf-pics-marketplace.appspot.com',
+  messagingSenderId: '246353836103',
+  appId: '1:246353836103:web:c13fb5a7463ef09bda9962',
+  measurementId: 'G-S51RGG3WFC',
 };
 export default firebaseConfig;
